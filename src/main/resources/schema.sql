5be2ddc7a6dcca5b8eb6d80cf66d85579dd2bc3b
souleabdoul <souley.abdoulaziz@fiserv.com>
2023-11-12 20:44:38 -0500

    Added the sql script to insert all the product at the first run of the program

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,13 @@ zip_code VARCHAR(255) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS product (product_id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL,
 price DECIMAL , product_size VARCHAR(255) NOT NULL);
+
+
+INSERT INTO product (product_id,name,price,product_size) VALUES
+(1,'Soda', 3.25, '2 Liter bottle'),
+(2,'Pizza - small Pepperoni', 7.35, '8 inch'),
+(3,'Pizza - medium Pepperoni', 9.35, '12 inch'),
+(4,'Pizza - large Pepperoni', 14.00, '18 inch'),
+(5,'Pizza - small house special', 8.50, '8 inch'),
+(6,'Pizza - medium house special', 10.50, '12 inch'),
+(7,'Pizza - large house special', 16.00, '18 inch');
